c723af1784ed4c1bba1c2f134d6b5bd507d90ebf
mgwer <nikita.mine134@gmail.com>
2025-11-19 18:03:36 +0300

    add flights list style

--- AviaFinder/AviaFinder/settings.py
+++ AviaFinder/AviaFinder/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Application definition
 
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticroot")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'/static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
