fecc23644d8cda6348a5786e5880d91fa25c0726
mgwer <nikita.mine134@gmail.com>
2025-12-09 17:00:57 +0300

    add logo and update flight card

--- AviaFinder/flights/models.py
+++ AviaFinder/flights/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import timedelta
 
 class City(models.Model):
     name = models.CharField(max_length=100)
@@ -23,6 +23,16 @@ class Flight(models.Model):
     arrival_time = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     transfers = models.IntegerField(default=0)
+
+    @property
+    def flight_time(self):
+        result = self.arrival_time - self.departure_time
+        if result.total_seconds() < 0:
+            result += timedelta(days=1)
+        total_minutes = result.total_seconds() // 60
+        hours = int(total_minutes // 60)
+        minutes = int(total_minutes % 60)
+        return f"{hours} ч {minutes} мин"
 
     def __str__(self):
         return f"{self.origin}-{self.destination}-{self.airline}"
